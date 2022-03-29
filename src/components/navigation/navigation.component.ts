f325e3a4de2c63e893ed518742a6f2883a739a5c
GaborFarkas <raandal73@gmail.com>
2022-03-29 13:29:01 +0200

    Add user model to navigation

--- src/components/navigation/navigation.component.ts
+++ src/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'navigation',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+    /**
+     * The logged in user. Null, if there is no logged in user.
+     */
+    public user?: User = {
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+
+    /**
+     * Get the display name of the logged in user. First name abbreviated (e.g. John Doe -> J. Doe).
+     * @returns
+     */
+    public getDisplayName(): string {
+        let name: string = '';
+
+        if (this.user) {
+            name = this.user.firstName[0].toUpperCase();
+            name += '. ';
+            name += this.user.lastName;
+        }
+
+        return name;
+    }
 }
