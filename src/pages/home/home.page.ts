79dd4dcac6a5f630c07ce6d82cc1caf011b17440
GaborFarkas <raandal73@gmail.com>
2022-03-29 13:30:28 +0200

    Add sample products to home page

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/models/product.model';
+import { ProductService } from 'src/services/product.service';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.css']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    private productService: ProductService = new ProductService();
+
+    public products: Product[] = [];
+
+    ngOnInit(): void {
+        this.products = this.productService.getProducts();
+    }
 }
