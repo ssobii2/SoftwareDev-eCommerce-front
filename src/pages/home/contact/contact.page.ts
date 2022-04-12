eaecfc7508237e96b00b3558e08aa21631f12328
THUNDER-PC\subha <rntvtf@pte.hu>
2022-04-12 12:12:31 +0200

    updating to latest code

---
+++ src/pages/home/contact/contact.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-page',
+  templateUrl: './contact.page.html',
+  styleUrls: ['./contact.page.css']
+})
+export class ContactsPage {
+}
---
+++ src/pages/home/products/products.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-page',
+  templateUrl: './products.page.html',
+  styleUrls: ['./products.page.css']
+})
+export class ProductsPage {
+}
